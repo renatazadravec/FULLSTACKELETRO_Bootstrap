6e7c88de5b8c253bf1300e6f3805c795575921a9
renatazadravec <renatasilva2512@gmail.com>
2020-11-02 19:48:23 -0300

    Inclusão de Bootstrap na Página Lojas

--- loja.php
+++ loja.php
@@ -2,33 +2,50 @@
 
 <section id="nossas-lojas">
 
-  <h2>Lojas</h2>
-  <hr />
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h2><b>Lojas</b></h2><hr>
+      </div>
+    </div>
+  </div>
 
   <div class="lojas">
-    <div class="loja">
-      <h3>Rio de Janeiro</h3>
-      <p>Avenida Presidente Vargas, 5000</p>
-      <p>10 &ordm; andar</p>
-      <p>Centro</p>
-      <p>(21) 3333-3333</p>
-    </div>
+    <div class="container">
+      <div class="row">
 
-    <div class="loja">
-      <h3>São Paulo</h3>
-      <p>Avenida Vargas, 5000</p>
-      <p>10 &ordm; andar</p>
-      <p>Centro</p>
-      <p>(21) 3333-3333</p>
-    </div>
+        <div class="col-md-4">
+          <div class="loja">
+            <h3>Rio de Janeiro</h3>
+            <p>Avenida Presidente Vargas, 5000</p>
+            <p>10 &ordm; andar</p>
+            <p>Centro</p>
+            <p>(21) 3333-3333</p>
+          </div>
+        </div>
 
-    <div class="loja">
-      <h3>Santa Catarina</h3>
-      <p>Avenida Presidente, 5000</p>
-      <p>10 &ordm; andar</p>
-      <p>Centro</p>
-      <p>(21) 3333-3333</p>
-    </div>
+        <div class="col-md-4">
+          <div class="loja">
+            <h3>São Paulo</h3>
+            <p>Avenida Vargas, 5000</p>
+            <p>10 &ordm; andar</p>
+            <p>Centro</p>
+            <p>(21) 3333-3333</p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="loja">
+            <h3>Santa Catarina</h3>
+            <p>Avenida Presidente, 5000</p>
+            <p>10 &ordm; andar</p>
+            <p>Centro</p>
+            <p>(21) 3333-3333</p>
+          </div>
+        </div>
+
+      </div>
+    </div> <!--container -->
   </div>
 </section>
 
