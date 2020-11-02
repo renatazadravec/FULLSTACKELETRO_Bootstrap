90130811fd6507141ef293109ad14ae4537471d9
renatazadravec <renatasilva2512@gmail.com>
2020-11-02 20:10:00 -0300

    Inclusão de Bootstrap na Página Contatos

--- contato.php
+++ contato.php
@@ -1,20 +1,33 @@
 <?php include_once("header.php"); ?>
 
 <section id="pagina-contato">
-  <h2>Contatos</h2>
-  <hr />
-
-  <div class="contatos">
-    <div class="contato">
-      <img src="./imagens/email.jpg"/>
-      <p>contato@fullstackeletro.com</p>
-    </div>
-
-    <div class="contato">
-      <img src="./imagens/whatsapp.png"/>
-      <p>(11) 99999-9999</p>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h2><b>Contatos</b></h2><hr>
+      </div>
     </div>
   </div>
+
+  <div class="contatos">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="contato">
+            <img src="./imagens/email.jpg"/>
+            <p>contato@fullstackeletro.com</p>
+          </div>
+        </div>
+        <div class="col">
+          <div class="contato">
+            <img src="./imagens/whatsapp.png"/>
+            <p>(11) 99999-9999</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <?php
   if(isset($_POST['nome']) && isset($_POST['mensagem'])){
     $nome = $_POST['nome'];
@@ -24,15 +37,25 @@
     $result = $conn->query($sql);
   }
   ?>
-  <form method="post" action="">
-    <label for="nome">Nome:</label>
-    <input type="text" id="nome" name="nome" class="input" />
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <form method="post" action="">
+          <div class="form-group">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" class="input form-control" />
+          </div>
 
-    <label for="mensagem">Mensagem:</label>
-    <textarea name="mensagem" id="mensagem" class="textarea"> </textarea>
+          <div class="form-group">
+            <label for="mensagem">Mensagem:</label>
+            <textarea name="mensagem" id="mensagem" class="textarea form-control"> </textarea>
+          </div>
 
-    <input type="submit" class="submit" value="Enviar" />
-  </form>
+          <input type="submit" class="btn submit" value="Enviar" />
+        </form>
+      </div>
+    </div>
+  </div>
 </section>
 
-    <?php include_once("footer.php"); ?>
+<?php include_once("footer.php"); ?>
