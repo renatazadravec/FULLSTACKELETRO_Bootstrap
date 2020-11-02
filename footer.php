ad145a99e719904eadcbe28e3e889861ad30e754
renatazadravec <renatasilva2512@gmail.com>
2020-11-02 19:26:59 -0300

    Inclusão Bootstrap no Footer

--- footer.php
+++ footer.php
@@ -1,15 +1,29 @@
     <div class="clear-both"></div>
     <footer>
-      <div>
-        <h4 class="forma-de-pagamento">
-          Formas de Pagamento
-        </h4>
-        <img
-          src="./imagens/formas de pagamento.png"
-          alt="Formas de Pagamento"
-        />
+      <div class="container">
+        <div class="row  my-2">
+          <div class="col">
+            <h4 class="text-center forma-de-pagamento">Forma de Pagamento</h4>
+          </div>
+        </div>
+
+        <div class="row  my-2">
+          <div class="col-md-4"></div>
+          <div class="col-md-4">
+            <img src="./imagens/formas de pagamento.png" alt="Formas de Pagamento" class="img-fluid"/>
+          </div>
+          <div class="col-md-4"></div>
+        </div>
       </div>
-      <div class="barra-footer">&copy; Recode Pro </div>
+
+  
+      <div class="barra-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col text-center"> &copy; Recode Pro </div>
+          </div>
+        </div>
+     </div>
     </footer>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" ></script>
