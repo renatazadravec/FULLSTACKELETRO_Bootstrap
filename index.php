070e4f27480692bcd237b45261a1d907eaf17081
renatazadravec <renatasilva2512@gmail.com>
2020-11-02 19:37:24 -0300

    Inclusão do Bootstrap na Home

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php include_once("header.php"); ?>
 
 <main>
-  <h4>Seja bem vindo (a)!</h4>
-  <p>
-    Aqui em nossa loja, <em>programadores tem desconto</em> nos produtos
-    para sua casa!
-  </p>
+<div class="jumbotron">
+  <div class="container">
+    <h3><b>Seja bem vindo (a)!</b></h3>
+    <p class="lead">Aqui em nossa loja, <em>programadores tem desconto</em> nos produtos
+    para sua casa!</p>
+  </div>
+</div>
 </main>
 
 <?php include_once("footer.php"); ?>
