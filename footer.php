863624bcd008930fea4a569237e36ce2d61e0e06
renatazadravec <renatasilva2512@gmail.com>
2020-11-02 18:48:51 -0300

    Atualização do Header e Separação do Footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+    <div class="clear-both"></div>
+    <footer>
+      <div>
+        <h4 class="forma-de-pagamento">
+          Formas de Pagamento
+        </h4>
+        <img
+          src="./imagens/formas de pagamento.png"
+          alt="Formas de Pagamento"
+        />
+      </div>
+      <div class="barra-footer">&copy; Recode Pro </div>
+    </footer>
+  </body>
+  <script src="js/funcoes.js"></script>
+</html>
